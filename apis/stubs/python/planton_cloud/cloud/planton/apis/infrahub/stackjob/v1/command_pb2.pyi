4921fc64d813bdf1dfbf41049a45a2dea989d029
sureshattaluri <suresh.attaluri789@gmail.com>
2025-10-31 02:44:09 +0530

    chore(repo): remove unused proto dependencies and generation infrastructure

--- apis/stubs/python/planton_cloud/cloud/planton/apis/infrahub/stackjob/v1/command_pb2.pyi
+++
@@ -1,9 +0,0 @@
-from cloud.planton.apis.commons.apiresource import rpc_service_options_pb2 as _rpc_service_options_pb2
-from cloud.planton.apis.infrahub.cloudresource.v1 import stack_pb2 as _stack_pb2
-from cloud.planton.apis.infrahub.stackjob.v1 import api_pb2 as _api_pb2
-from cloud.planton.apis.infrahub.stackjob.v1 import io_pb2 as _io_pb2
-from cloud.planton.apis.infrahub.stackjob.v1 import progress_pb2 as _progress_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
--- apis/stubs/python/planton_cloud/cloud/planton/apis/search/v1/infrahub/cloudresource/query_pb2.pyi
+++
@@ -1,8 +0,0 @@
-from cloud.planton.apis.commons.apiresource import io_pb2 as _io_pb2
-from cloud.planton.apis.iam.iampolicy.v1.rpcauthorization import method_options_pb2 as _method_options_pb2
-from cloud.planton.apis.search.v1.apiresource import record_pb2 as _record_pb2
-from cloud.planton.apis.search.v1.infrahub.cloudresource import io_pb2 as _io_pb2_1
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
